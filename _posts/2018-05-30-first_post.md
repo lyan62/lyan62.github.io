d3cda80fde914e8e7cc41d5145a782fc1a1f7364
Wenyan Li <wenyanl62@gmail.com>
2018-05-31 12:42:30 -0400

    tryout markdown learning notes

--- _posts/2018-05-30-first_post.md
+++
@@ -1,7 +0,0 @@
----
-layout: single
-title:  "My first post"
-date:   2018-05-30
----
-
-my first post looks just fine
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "My first post",
-        "excerpt":"my first post looks just fine ","categories": [],
+        "title": "NOTES: EPI CH4 Primitive Types",
+        "excerpt":"Primitive Types 4.1 Parity Parity fo a binary word is 1 if the number of 1s in the word is odd, o.w. is 0. def parity(x): # brute force result = 0 while x: result ^= x &amp; 1 x &gt;&gt;= 1 return result # O(n)#------------------------ improve--------------------------## Trick: x &amp;...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/first_post/",
+        "url": "http://localhost:4000/EPI04_primitive_types/",
         "teaser":null}]
